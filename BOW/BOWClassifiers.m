f6972ff14aad19820a3001385b03ab40dff17fae
CostatoMarco <m.costato@campus.unimib.it>
2024-12-24 15:56:52 +0100

    Risultati Classificazione BOW

--- BOW/BOWClassifiers.m
+++ BOW/BOWClassifiers.m
@@ -1,1 +1,26 @@
-[trainds, unlabledds, valds, degds] = datastoreCreator();
+%{
+load("trainds.mat")
+load("valds.mat")
+disp("Making BOWs")
+[BOW_tr,labels_tr,BOW_val,labels_val] = BOWCreator(trainds,valds,20,200,["SURF"],150);
+disp("Training")
+tb = TreeBagger(200,BOW_tr, labels_tr);
+svm = fitcecoc(BOW_tr,labels_tr);
+disp("Predicting")
+predicted = cellfun(@str2double,tb.predict(BOW_val));
+%}
+CM = confusionmat(labels_val,categorical(predicted));
+CM = CM./repmat(sum(CM,2),1,size(CM,2));
+accuracy = mean(diag(CM));
+figure(1)
+confusionchart(CM)
+title(["Accuracy:", num2str(accuracy), "20,200,[SURF],150, TreeBagger200"])
+
+predicted = svm.predict(BOW_val);
+CM = confusionmat(labels_val,predicted);
+CM = CM./repmat(sum(CM,2),1,size(CM,2));
+accuracy = mean(diag(CM));
+
+figure(2)
+confusionchart(CM)
+title(["Accuracy:", num2str(accuracy), "20,200,[SURF],150, SVM"])
