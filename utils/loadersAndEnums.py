e17a4e7fc611a42a073ed4966bedf85b6f8afdfb
AAAlessandroP <a@b.c>
2025-01-02 16:49:15 +0100

    externalization and modifying ImageDataset to account for the ventual augm pipe

--- utils/loadersAndEnums.py
+++ utils/loadersAndEnums.py
@@ -13,6 +13,9 @@ class datasets(Enum):
     TEST = ["val_info.csv", "val_set"]
     TEST_DEGRADED = ["val_info.csv", "val_set_degraded"]
 
+
+#https://pytorch.org/hub/pytorch_vision_alexnet/
+# All pre-trained models expect input images normalized in the same way, i.e. mini-batches of 3-channel RGB images of shape (3 x H x W), where H and W are expected to be at least 224. The images have to be loaded in to a range of [0, 1] and then normalized using mean = [0.485, 0.456, 0.406] and std = [0.229, 0.224, 0.225].
 class networks(Enum):
     ALEXNET = [227, models.alexnet(pretrained=True), "AlexNet"]
     RESNET50 = [224, models.resnet50(pretrained=True), "ResNet50"]
@@ -37,10 +40,11 @@ class ImageDataset(Dataset):
         print(image_np)
         plt.axis('off') # Hide the axis plt.show()
 
-    def __init__(self, dataset, network_input_size, cuda):
+    def __init__(self, dataset, network_input_size, cuda, transform=None):
         super().__init__()
         self.images_names = []
         self.labels = []
+        self.transform = transform
         self.cuda = cuda
         dataset = dataset.value
         annotations_file = dataset[0]
@@ -64,8 +68,12 @@ class ImageDataset(Dataset):
                                         interpolation=cv2.INTER_CUBIC).astype(np.float32)
         image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
         #moveaxis serve per avere come dimensione dell'immagine (3, righe, colonne) invece di (righe, colonne, 3)
-        image = np.moveaxis(image_rgb, -1, 0)
-        # eventualmente si può aggiungere l'alternativa di fare random cropping dell'immagine
+        if self.transform is None:
+            image = np.moveaxis(image_rgb, -1, 0)
+        else:  #apply the transformation pipe         
+            image = self.transform(image=image_rgb)["image"] #anche se l'input è (256, 256, 3), restutuisce in formato torch.Size([3, 256, 256]) o comunque (3, h, w) se la pipeline fa crop/altro
+
+        
         label = self.labels[index]
         if(self.cuda):
             return torch.from_numpy(image).cuda(), label
